abeca2692c92801932a93c3dff2310475823f342
Padu76 <andrea.padoan@gmail.com>
2025-07-14 10:03:33 +0200

    Add files via upload

--- components/providers/Providers.tsx
+++ components/providers/Providers.tsx
@@ -1,11 +1,11 @@
 'use client'
 
-import { SessionProvider } from 'next-auth/react'
+import { SessionProvider, Session } from 'next-auth/react'
 import { ReactNode } from 'react'
 
 interface ProvidersProps {
   children: ReactNode
-  session?: any
+  session?: Session | null
 }
 
 export default function Providers({ children, session }: ProvidersProps) {
