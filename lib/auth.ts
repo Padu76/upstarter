a1e5c6db6da838d6509461d1b1fdf4702ee4f241
Padu76 <andrea.padoan@gmail.com>
2025-07-14 17:30:15 +0200

    Add files via upload

--- lib/auth.ts
+++ lib/auth.ts
@@ -51,7 +51,7 @@ export const authOptions: NextAuthOptions = {
       return token
     },
     async session({ session, token }) {
-      if (token) {
+      if (token && session.user) {
         session.user.email = token.email as string
         session.user.name = token.name as string
       }
